a3d279aa4729ee5632e906b1e138de36f80ab7fe
Jia-Shen Boon <jiashen@gmail.com>
2015-03-07 23:47:57 -0600

    Use smoother alpha blender in pipeline

--- translationStitch.m
+++ translationStitch.m
@@ -63,38 +63,14 @@ end
 
 %% Equalize image exposures to reduce sharp changes in brightness when stitching
 
-% Find the average brightness of each image compared to some global
-% standard
-ratios = 1;
-for i = 1 : numImages - 1
-   im1 = rgb2gray(imtranslateds{i});
-   im2 = rgb2gray(imtranslateds{i + 1});
-   overlap = im1 > 0 & im2 > 0;
-   ratio = sum(im2(overlap)) / sum(im1(overlap));
-   ratios(i + 1) = ratio;
-end
-global_exposures = cumprod(ratios);  % low value means image too dark
- 
-% Set median exposure to be 1 so that average exposure isn't too bright or
-% dark
-global_exposures = global_exposures / median(global_exposures);
-% global_exposures = global_exposures / min(global_exposures);
- 
-% Equalize exposure individually
-for i = 1 : numImages
-      % Average ratio with 1 so that we don't make the compensation too
-      % extreme. Results seem better if we don't go all out with
-      % global_exposures(i)
-    divisor = (global_exposures(i) + 1) / 2;
-    imtranslateds{i} = uint8(double(imtranslateds{i}) / divisor);
-end
+equalizeExposures;
  
 %% Perform stitching by alpha blending input images 1 by 1 to panorama
 imPanorama = imtranslateds{1};
 for i = 2 : numImages
     
     imtranslated = imtranslateds{i};
-    imPanorama = alpha_blend(imPanorama, imtranslated);
+    imPanorama = alphaBlendSmooth(imPanorama, imtranslated);
 end
 
 % Display translated images in a single figure
