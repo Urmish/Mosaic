cf2d673f587c8594d417f2956a1687b0dcf0c503
Jia-Shen Boon <jiashen@gmail.com>
2015-03-07 23:47:09 -0600

    Refactor exposure equalization into its own script

---
+++ equalizeExposures.m
@@ -0,0 +1,32 @@
+%% Equalize image exposures to reduce sharp changes in brightness when stitching
+% Assumes there is a imtranslateds cell array which we'll equalize
+
+if ~exist(imtranslateds, 'var')
+    error('Script expects a cell array imtranslateds');
+end
+
+% Find the average brightness of each image compared to some global
+% standard
+ratios = 1;
+for i = 1 : numImages - 1
+   im1 = rgb2gray(imtranslateds{i});
+   im2 = rgb2gray(imtranslateds{i + 1});
+   overlap = im1 > 0 & im2 > 0;
+   ratio = sum(im2(overlap)) / sum(im1(overlap));
+   ratios(i + 1) = ratio;
+end
+global_exposures = cumprod(ratios);  % low value means image too dark
+ 
+% Set median exposure to be 1 so that average exposure isn't too bright or
+% dark
+global_exposures = global_exposures / median(global_exposures);
+% global_exposures = global_exposures / min(global_exposures);
+ 
+% Equalize exposure individually
+for i = 1 : numImages
+      % Average ratio with 1 so that we don't make the compensation too
+      % extreme. Results seem better if we don't go all out with
+      % global_exposures(i)
+    divisor = (global_exposures(i) + 1) / 2;
+    imtranslateds{i} = uint8(double(imtranslateds{i}) / divisor);
+end
