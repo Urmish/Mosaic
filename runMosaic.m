26e40cf2b41dbf07b5a8a3132857f08214d8f76b
Jia-Shen Boon <jiashen@gmail.com>
2015-03-16 17:52:51 -0500

    Refactor homography matching

--- runMosaic.m
+++ runMosaic.m
@@ -14,7 +14,7 @@ img = ReadImagesFromFolder(imageFolder);
 img = uint8(img);  % img is supposed to be uint8 upon loading but it's not. Oh well.
 
 % Subsample the number of images
-img = img(:, :, :, 1:2);
+% img = img(:, :, :, 1:2);
 numImages = size(img, 4);
 
 % numImages = 4;
@@ -34,7 +34,7 @@ IS_S110 = strcmp(paramFile, 'data/s110Params.txt');
 % k1 = -0.15; k2 = 0.0;  % camera parameters for radial distortion (Jia's test images)
 % focalLength = 595;  % Jia's test image
 params = dlmread(paramFile);
-k1 = params(1); k2 = params(2), focalLength = params(3);
+k1 = params(1); k2 = params(2); focalLength = params(3);
 
 if IS_S110
     factor = size(img, 1) / 4000;  % how much large this image set is c.f. 640
@@ -73,16 +73,13 @@ end
 %% Compute homography from SIFT descriptors
 HomographyMatrix = {};
 HomographyMatches = {};
-for i=1:length(cylindricalImage)-1
-    [HomographyMatrix_temp, HomographyMatches_temp] = Ransac(siftFeatures{i}, siftDescriptors{i},siftFeatures{i+1}, siftDescriptors{i+1});
-    sprintf('Ransac for %d & %d image is over',i,i+1)
+for i=1:length(cylindricalImage)
+    i2 = mod(i, length(cylindricalImage)) + 1;
+    [HomographyMatrix_temp, HomographyMatches_temp] = Ransac(siftFeatures{i}, siftDescriptors{i},siftFeatures{i2}, siftDescriptors{i2});
+    fprintf('Ransac for %d & %d image is over\n',i,i2)
     HomographyMatches{i} = HomographyMatches_temp;
     HomographyMatrix{i} = HomographyMatrix_temp;
 end
-[HomographyMatrix_temp, HomographyMatches_temp] = Ransac(siftFeatures{length(cylindricalImage)}, siftDescriptors{length(cylindricalImage)},siftFeatures{1}, siftDescriptors{1});
-sprintf('Ransac for %d & %d image is over',length(cylindricalImage),1)
-HomographyMatches{length(cylindricalImage)} = HomographyMatches_temp;
-HomographyMatrix{length(cylindricalImage)} = HomographyMatrix_temp;
 
 %Code for stitching goes here
 %panImg = StitchEmUp(cylindricalImage, Mask, HomographyMatrix);
